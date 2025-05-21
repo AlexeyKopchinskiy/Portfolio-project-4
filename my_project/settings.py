5a5143b3586cecfd7fe5042126d9fa83eb4b4648
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-05-21 15:09:02 +0200

    Restore original secret key

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jm#cr^o+#l=l%)42ql$p_)8_noh)q_e8q*#lh8v+nq#_qkbd(c'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
