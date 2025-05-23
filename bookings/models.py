ed0324c9378f2563d87a361accd31525b0fc77a4
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-05-23 14:22:34 +0200

    Fix location attribute in table module

--- bookings/models.py
+++ bookings/models.py
@@ -21,6 +21,18 @@ class Reservations(models.Model):
         return f"Booking by {self.user_id.username} on {self.booking_date}"
 
 
+# Define Tables Table
+
+
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)  # Unique table number
+    capacity = models.IntegerField()  # Number of seats per table
+    location = models.ForeignKey(
+        "Location", on_delete=models.CASCADE, related_name="tables")
+
+    def __str__(self):
+        return f"Table {self.table_number} ({self.location.location_name})"
+
 # Define Locations Table
 
 
@@ -30,15 +42,3 @@ class Location(models.Model):
 
     def __str__(self):
         return f"Location {self.location_name}"
-
-# Define Tables Table
-
-
-class Table(models.Model):
-    table_number = models.IntegerField(unique=True)  # Unique table number
-    capacity = models.IntegerField()  # Number of seats per table
-    location = models.ForeignKey(
-        Location, on_delete=models.CASCADE, related_name="tables")
-
-    def __str__(self):
-        return f"Table {self.table_number} ({self.location.name})"
